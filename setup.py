5422d6964643eda24fddb3e5a0fd8035f91832ee
Dennis <dennis@simula.no>
2023-10-16 22:22:11 +0200

    New Status

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt", "r") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="my_package",
+    name="gimitest",
     version="0.1",
     packages=find_packages(),
     install_requires=requirements,
