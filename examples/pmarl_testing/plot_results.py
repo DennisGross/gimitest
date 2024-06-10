da14f612156addb57c9e4d725fe62bbd2b32fb73
Dennis <dennis@simula.no>
2024-06-10 13:22:09 +0200

    Added new plotting scripts

--- examples/pmarl_testing/plot_results.py
+++ examples/pmarl_testing/plot_results.py
@@ -2,13 +2,22 @@ import matplotlib.pyplot as plt
 import pandas as pd
 df = pd.read_csv("waterworld_evaders_reward.csv")
 plt.scatter(df["n_evaders"], df["n_poisons"], c=df["collected_reward"])
-plt.xlabel("Number of evaders")
-plt.ylabel("Number of poisons")
+plt.xlabel("Number of evaders", fontsize=20)
+plt.ylabel("Number of poisons", fontsize=20)
+
 # Colorbar
 cbar = plt.colorbar()
-cbar.set_label("Collected reward")
+# Colorbar size
+cbar.set_label("Collected reward", fontsize=15)
+
 #plt.title("Waterworld evaders reward")
 plt.tight_layout()
 plt.grid(alpha=0.2)
+
+# Set the font size for the tick labels
+plt.xticks(fontsize=15)
+plt.yticks(fontsize=15)
+# Legend font size
+
 plt.savefig("waterworld_evaders_reward.png")
 plt.savefig("waterworld_evaders_reward.eps")
--- examples/tmarl_testing/plot_results.py
+++ examples/tmarl_testing/plot_results.py
@@ -1,11 +1,27 @@
 # Plot the winrate for each init_coin value in a histogram
 import matplotlib.pyplot as plt
 import pandas as pd
+
+# Load the data
 df = pd.read_csv("connect_four_init_coin_winrate.csv")
-plt.figure(figsize=(6.4, 2.4))
+
+# Create a figure with specific size
+plt.figure(figsize=(6.4, 3))
+
+# Plot the bar chart
 plt.bar(df["init_coin"], df["winrate"])
-plt.xlabel("Initial Column of Player 1's first Coin")
-plt.ylabel("Winrate Player 2")
+
+# Set labels for the axes
+plt.xlabel("Initial Column of Player 1's first Coin", fontsize=20)
+plt.ylabel("Winrate Player 2", fontsize=20)
+
+# Set the font size for the tick labels
+plt.xticks(fontsize=15)
+plt.yticks(fontsize=15)
+
+# Ensure the layout is tight
 plt.tight_layout()
+
+# Save the figure in different formats
 plt.savefig("connect_four_init_coin_winrate.png")
 plt.savefig("connect_four_init_coin_winrate.eps")
